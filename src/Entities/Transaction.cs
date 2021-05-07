4a05627006a653f84213a75519a6de00de9ba9e7
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2021-05-07 23:42:41 +0200

    Fix error with swapped public/secret keys

--- src/Entities/Transaction.cs
+++ src/Entities/Transaction.cs
@@ -2,10 +2,10 @@ namespace Entities
 {
     internal class Transaction
     {
-        // pubKey = the public key of the identity issuing the transaction
+        // s = the secret key of the identity issuing the transaction
         // m = some agreed upon combination of data from transaction
         // n = some agreed upon arbitrary number
-        // (pubKey * m) mod n
+        // (s * m) mod n
         public string Signature { get; set; }
     }
 }
